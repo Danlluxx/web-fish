ba466eb503f5aca673955b9408d123c9f4aa4bbf
Danlluxx <fanatbmw04@mail.ru>
2026-04-29 02:33:13 +0700

    Fix production site URL for sitemap

--- lib/site.ts
+++ lib/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: "AquaMarket",
   description:
     "Интернет-магазин аквариумных обитателей и растений с ассортиментом более 350 видов. Большой выбор, выгодные цены, удобный Excel-прайс и отправка заказов по всей России.",
-  siteUrl: "https://example.com",
+  siteUrl: "https://vs-o-rybkah.ru",
   phoneHref: "tel:+79130220796",
   phoneLabel: "+7 913 022 0796",
   telegramUrl: "https://t.me/VS_o_rybkah",
